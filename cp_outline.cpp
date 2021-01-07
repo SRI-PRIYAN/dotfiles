faa4d6b71c13061def8ead57bf91b0a77d28c09f
SRI-PRIYAN <sripriyans01@gmail.com>
2021-01-07 15:03:37 +0530

    added -cp flag to out function for creating files specific to competitive programming

---
+++ cp_outline.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithms>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        
+    }
+
+	return 0;
+}
