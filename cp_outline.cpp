90d6b5b5a99baa9888dbac05cf39a63201d1caed
SRI-PRIYAN <sripriyans01@gmail.com>
2021-01-30 14:23:06 +0530

    Adding ceil and isPrime functions

--- cp_outline.cpp
+++ cp_outline.cpp
@@ -16,6 +16,12 @@ using namespace std;
 
 typedef long long ll;
 
+template<typename T>
+T ceil(T, T);
+
+template<typename T>
+bool isPrime(T);
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -29,3 +35,22 @@ int main() {
 
 	return 0;
 }
+
+template<typename T>
+T ceil(T a, T b) {
+    return (a / b) + (a % b != 0);
+}
+
+template<typename T>
+bool isPrime(T n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3) return true;
+
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (T i = 5; i * i <= n; i+=6) {
+        if ((n % i == 0) || (n % (i + 2) == 0)) return false;
+    }
+
+    return true;
+}
+
