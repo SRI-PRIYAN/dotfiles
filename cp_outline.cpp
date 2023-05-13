d34d4b01c3257a5b02a7c39f1eda409e084b1913
SRI-PRIYAN <sripriyans01@gmail.com>
2023-05-13 12:21:00 +0530

    Fix indentation

--- cp_outline.cpp
+++ cp_outline.cpp
@@ -12,34 +12,34 @@ template<typename T>
 bool isPrime(T);
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    int t;
-    cin >> t;
+  int t;
+  cin >> t;
 
-    while (t--) {
-        
-    }
+  while (t--) {
 
-	return 0;
+  }
+
+  return 0;
 }
 
 template<typename T>
 T ceil(T a, T b) {
-    return (a / b) + (a % b != 0);
+  return (a / b) + (a % b != 0);
 }
 
 template<typename T>
 bool isPrime(T n) {
-    if (n <= 1) return false;
-    if (n == 2 || n == 3) return true;
+  if (n <= 1) return false;
+  if (n == 2 || n == 3) return true;
 
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (T i = 5; i * i <= n; i+=6) {
-        if ((n % i == 0) || (n % (i + 2) == 0)) return false;
-    }
+  if (n % 2 == 0 || n % 3 == 0) return false;
+  for (T i = 5; i * i <= n; i+=6) {
+    if ((n % i == 0) || (n % (i + 2) == 0)) return false;
+  }
 
-    return true;
+  return true;
 }
 
